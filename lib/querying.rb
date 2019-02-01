16a274eff4f347fcc988c0cf6f88fdd1893bc35f
Adrienne Yao <adrienne.yao@icloud.com>
2019-02-01 03:48:02 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,9 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT species, count(species)
+   FROM characters
+   WHERE count(species) = (SELECT max(count(species)) FROM characters);"
 end
 
 def select_name_and_series_subgenres_of_authors
