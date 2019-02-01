25423296bd770b81b25003e7535d5cd2f3ffaa9b
Adrienne Yao <adrienne.yao@icloud.com>
2019-02-01 03:48:25 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ end
 def select_value_and_count_of_most_prolific_species
   "SELECT species, count(species)
    FROM characters
-   WHERE count(species) = (SELECT max(count(species)) FROM characters);"
+   HAVING count(species) = (SELECT max(count(species)) FROM characters);"
 end
 
 def select_name_and_series_subgenres_of_authors
