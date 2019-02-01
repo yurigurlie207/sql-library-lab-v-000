09cfef23da15709a6f0057368b575a36a60e3554
Adrienne Yao <adrienne.yao@icloud.com>
2019-02-01 05:19:13 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -43,5 +43,5 @@ def select_character_names_and_number_of_books_they_are_in
    INNER JOIN character_books
    ON characters.id = character_books.character_id
    GROUP BY character_id
-   ORDER BY Books, charcters.name DESC;"
+   ORDER BY Books, characters.name DESC;"
 end
