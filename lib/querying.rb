0b9393131debb3c7a1119764e2232abe7dbb10f6
Adrienne Yao <adrienne.yao@icloud.com>
2019-02-01 03:38:16 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,5 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  ""
+  "SELECT title, year
+   FROM books
+   WHERE series_id = 1
+   ORDER BY year;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
