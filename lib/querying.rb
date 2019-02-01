6c165e0ca6e7f4c20846e5b7e7fc41ea87c9ac06
Adrienne Yao <adrienne.yao@icloud.com>
2019-02-01 04:46:51 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,9 +36,7 @@ def select_series_title_with_most_human_characters
                HAVING count(species) =
                (SELECT);
 
-0. find max human species grouped by series
-1. select the series id with max human species
-2. select title of series
+?
 
 end
 
