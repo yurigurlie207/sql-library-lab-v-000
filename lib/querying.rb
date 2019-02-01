397056c58997b057db5644a76689e20c78bcaa53
Adrienne Yao <adrienne.yao@icloud.com>
2019-02-01 04:21:11 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,7 @@ def select_name_and_series_subgenres_of_authors
    INNER JOIN series
    ON authors.id = series.author_id
    JOIN subgenres
-   ON subgenres.id = series.subgenre_id"
+   ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
