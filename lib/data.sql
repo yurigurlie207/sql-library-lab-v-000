f0a926692906da7cfed5ad12613e6130e8cf390a
Adrienne Yao <adrienne.yao@icloud.com>
2019-02-01 03:31:03 +0000

    Automatically backed up by Learn

--- lib/data.sql
+++ lib/data.sql
@@ -6,7 +6,9 @@ INSERT INTO subgenres (id, name)
 VALUES (1, "medieval"),
        (2, "space opera");
 
-INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
+INSERT INTO authors (id, name)
+VALUES (1, "George R. R. Martin"),
+       (2, "Second Author");
 
 INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
