d55c847a3f8f44e110668cf0013b5febe812358a
Adrienne Yao <adrienne.yao@icloud.com>
2019-02-01 05:15:36 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -38,5 +38,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT character.names, "
+  "SELECT character.names, count(book_id) as Books
+   FROM characters
+   INNER JOIN character_books
+   ON
+   GROUP BY character_id"
 end
